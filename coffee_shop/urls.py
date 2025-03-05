d2bc5e82824a771542ba95683ce296f8070c1483
Cristobal <quilimacox1@gmail.com>
2025-03-05 11:24:28 -0500

    se crea archivo gitignore

--- coffee_shop/urls.py
+++ coffee_shop/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for coffe_shop project.
+URL configuration for coffee_shop project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
