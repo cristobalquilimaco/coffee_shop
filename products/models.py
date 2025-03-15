d09c352f6680a8b9d1e49df78cf282db6c14c577
Cristobal <quilimacox1@gmail.com>
2025-03-15 10:01:27 -0500

    Se agrega nuevo campo para agregar la fecha de creacion

--- products/models.py
+++ products/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Precio")
     available = models.BooleanField(default=True, verbose_name="Disponible")
     photo = models.ImageField(upload_to="logos", null=True, blank=True, verbose_name="Foto")
+    creationAt = models.DateField(auto_now_add=True, verbose_name="Fecha de Creacion")
 
     def __str__(self):
         return self.name
