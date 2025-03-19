221f3c4f68e0e45658c4c1045f73aca480a0470b
Cristobal <quilimacox1@gmail.com>
2025-03-19 18:34:25 -0500

    Update Dajngo

--- users/urls.py
+++ users/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('login/', 
         LoginView.as_view(template_name="users/login.html"), 
         name="login"),
-    path('logout/', LogoutView)
+    path('logout/', LogoutView.as_view())
 ]
