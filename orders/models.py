2eaf5f246bffc448215791c1cd619ccef25e1826
Cristobal <quilimacox1@gmail.com>
2025-03-25 11:56:09 -0500

    Se agretga funcion __str__ con mensaje de la orden

--- orders/models.py
+++ orders/models.py
@@ -6,4 +6,8 @@ class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
     order_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"order {self.id} by {self.user}"
+    
     
